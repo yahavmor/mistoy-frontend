9de46125a8895af9c8b85a63ca2718251882e780
yahavmor <yahavmor77@gmail.com>
2025-12-02 18:55:57 +0200

    add global styles and component-specific styles; update main entry point to use new styles

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { toyService } from '../services/toy.service.js'
-import './index.css'
+import '../src/assets/styles/main.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App/>
   </StrictMode>,
 )
