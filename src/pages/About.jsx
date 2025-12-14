d5f2a71b3588d70eed8c3f1476ad7176091de142
yahavmor <yahavmor77@gmail.com>
2025-12-14 18:49:12 +0200

    editing the home page to be scary

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,19 @@
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 import { useEffect, useState } from "react";
 import { mapService } from "../../services/map.service.js";
 
-
-
 export function About() {
-    const [branches,setBraches]=useState([])
-    const [center,setCenter]=useState({ lat: 32.0853, lng: 34.7818 })
+  const [branches, setBranches] = useState([]);
+  const [center, setCenter] = useState({ lat: 32.0853, lng: 34.7818 });
 
-    useEffect(()=>{
-        setBraches(mapService.getBranches())
-    },[])
+  useEffect(() => {
+    setBranches(mapService.getBranches());
+  }, []);
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyDV32zrtz0rOxkzvuFRJJ1fDRknM5gqFMg",
+  });
+
   const containerStyle = {
     width: "100%",
     height: "700px",
@@ -18,25 +21,28 @@ export function About() {
 
   function onSelectBranch(branch) {
     setCenter({ lat: branch.lat, lng: branch.lng });
-}
+  }
 
+  if (!isLoaded) return <div>Loading map…</div>;
 
   return (
     <section className="about">
       <h1>Map of all Branches</h1>
-      <LoadScript googleMapsApiKey="AIzaSyDV32zrtz0rOxkzvuFRJJ1fDRknM5gqFMg">
-        <GoogleMap mapContainerStyle={containerStyle} zoom={12} center={center}>
 
-            {branches && branches.map((branch, idx) => (
-            <Marker
-              key={idx}
-              position={{ lat: branch.lat, lng: branch.lng }}
-              onClick={() => onSelectBranch(branch)}
-              label={branch.name} 
-            />
-            ))}
-        </GoogleMap>
-      </LoadScript>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        zoom={12}
+        center={center}
+      >
+        {branches.map((branch, idx) => (
+          <Marker
+            key={idx}
+            position={{ lat: branch.lat, lng: branch.lng }}
+            onClick={() => onSelectBranch(branch)}
+            label={branch.name}
+          />
+        ))}
+      </GoogleMap>
     </section>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export function Home() {
-    
-    return (
-        <section className="home">
-            <h1>Toys R Us!</h1>
-        </section>
-    )
+  return (
+    <section className="home">
+      <h1 className="home-title">Toys R Us!</h1>
+      <img className="home-image" src="scray home image.png" alt="home-image"/>
+    </section>
+  );
 }
